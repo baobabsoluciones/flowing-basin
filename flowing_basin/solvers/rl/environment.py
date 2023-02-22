Fixed income calculation + added simple lerp to calculate turbined flow (for TESTING)

flowing_basin/solvers/rl/environment.py
@@ -14,12 +14,12 @@ class DamObservation:
 
     vol: float
     lags: list[float]
-    unreg_flows: list[float]
+    next_unreg_flows: list[float]
     list: list = field(init=False)
 
     def __post_init__(self):
 
-        self.list = [self.vol, *self.lags, *self.unreg_flows]
+        self.list = [self.vol, *self.lags, *self.next_unreg_flows]
 
 
 @dataclass
@@ -31,8 +31,8 @@ class Observation:
     After calling the normalization method, the normalized tensor is saved in a `normalized` attribute
     """
 
-    prices: list[float]
-    incoming_flows: list[float]
+    next_prices: list[float]
+    next_incoming_flows: list[float]
     dams: list[DamObservation]
     tensor: torch.Tensor = field(init=False)
     normalized: None | torch.Tensor = field(init=False)
@@ -40,7 +40,7 @@ class Observation:
     def __post_init__(self):
 
         dam_values = [value for dam in self.dams for value in dam.list]
-        self.tensor = torch.tensor([*self.prices, *self.incoming_flows, *dam_values])
+        self.tensor = torch.tensor([*self.next_prices, *self.next_incoming_flows, *dam_values])
         self.normalized = None
 
     def normalize(self, low: "Observation", high: "Observation"):
@@ -103,13 +103,13 @@ class Environment:
         """
 
         return Observation(
-            prices=[0] * self.num_prices,
-            incoming_flows=[0] * self.num_incoming_flows,
+            next_prices=[0] * self.num_prices,
+            next_incoming_flows=[0] * self.num_incoming_flows,
             dams=[
                 DamObservation(
                     vol=self.instance.get_min_vol_of_dam(dam_id),
                     lags=[0] * self.instance.get_relevant_lags_of_dam(dam_id)[-1],
-                    unreg_flows=[0] * self.num_unreg_flows,
+                    next_unreg_flows=[0] * self.num_unreg_flows,
                 )
                 for dam_id, dam in self.river_basin.dams.items()
             ],
@@ -122,7 +122,7 @@ class Environment:
         """
 
         return Observation(
-            prices=[
+            next_prices=[
                 max(
                     self.instance.get_price(
                         0, num_steps=self.instance.get_num_time_steps()
@@ -130,14 +130,14 @@ class Environment:
                 )
             ]
             * self.num_prices,
-            incoming_flows=[self.instance.get_max_incoming_flow()]
+            next_incoming_flows=[self.instance.get_max_incoming_flow()]
             * self.num_incoming_flows,
             dams=[
                 DamObservation(
                     vol=self.instance.get_max_vol_of_dam(dam_id),
                     lags=[self.instance.get_max_flow_of_channel(dam_id)]
                     * self.instance.get_relevant_lags_of_dam(dam_id)[-1],
-                    unreg_flows=[self.instance.get_max_unregulated_flow_of_dam(dam_id)]
+                    next_unreg_flows=[self.instance.get_max_unregulated_flow_of_dam(dam_id)]
                     * self.num_unreg_flows,
                 )
                 for dam_id, dam in self.river_basin.dams.items()
@@ -151,17 +151,17 @@ class Environment:
         """
 
         obs = Observation(
-            prices=self.instance.get_price(
+            next_prices=self.instance.get_price(
                 self.river_basin.time, num_steps=self.num_prices
             ),
-            incoming_flows=self.instance.get_incoming_flow(
+            next_incoming_flows=self.instance.get_incoming_flow(
                 self.river_basin.time, num_steps=self.num_incoming_flows
             ),
             dams=[
                 DamObservation(
                     vol=dam.volume,
                     lags=dam.channel.flows_over_time.squeeze().tolist(),
-                    unreg_flows=self.instance.get_unregulated_flow_of_dam(
+                    next_unreg_flows=self.instance.get_unregulated_flow_of_dam(
                         self.river_basin.time, dam_id, num_steps=self.num_unreg_flows
                     ),
                 )
@@ -173,26 +173,6 @@ class Environment:
 
         return obs
 
-    def get_reward(self) -> float:
-
-        """
-        Returns the reward that should be given to the agent for the energy generated in the current time step
-
-        The reward is proportional to the income obtained, income (EUR) = price (EUR/MWh) * power (MW) * time_step (h)
-        However, the normalized energy price is used, to avoid inconsistencies throughout several episodes
-        (in which energy prices are normalized differently)
-        """
-
-        price_normalized = (
-            self.instance.get_price(self.river_basin.time) / self.obs_high.prices[0]
-        )
-        power = sum(
-            dam.channel.power_group.power for dam in self.river_basin.dams.values()
-        )
-        time_step_hours = self.instance.get_time_step() / 3600
-
-        return price_normalized * power * time_step_hours
-
     def step(self, action: torch.Tensor) -> tuple[float, Observation, bool]:
 
         """
@@ -203,9 +183,12 @@ class Environment:
         assert list(action.size()) == [self.instance.get_num_dams()]
 
         flows = action.tolist()
-        self.river_basin.update(flows)
+        income = self.river_basin.update(flows)
 
-        reward = self.get_reward()
+        # The reward is proportional to the income obtained
+        # However, the normalized energy price is used, to avoid inconsistencies throughout several episodes
+        # (in which energy prices are normalized differently)
+        reward = income / self.obs_high.next_prices[0]
         next_obs = self.get_observation()
         done = self.river_basin.time >= self.instance.get_num_time_steps()
 

flowing_basin/tests/test_river_basin.py
@@ -15,17 +15,33 @@ def test_river_basin(
 
     for i, flows in enumerate(decisions):
 
-        print(f"---- decision {i} ----")
+        print(f">>>> decision {i}")
         income = river_basin.update(flows)
+        print(f"income={income}")
         state = river_basin.get_state()
         print(f"state after decision {i}:", state)
+        income_calc = old_state['next_price'] * (state['dam1']['power'] + state['dam2']['power']) * river_basin.instance.get_time_step() / 3600
         print(
             f"income obtained after decision {i} (EUR): "
             f"price (EUR/MWh) * power (MW) * time_step (hours) = "
-            f"{state['price']} * ({state['dam1']['power']} + {state['dam2']['power']}) * {river_basin.instance.get_time_step() / 3600} = "
-            f"{state['price'] * (state['dam1']['power'] + state['dam2']['power']) * river_basin.instance.get_time_step() / 3600}"
+            f"{old_state['next_price']} * ({state['dam1']['power']} + {state['dam2']['power']}) * {river_basin.instance.get_time_step() / 3600} = "
+            f"{income_calc}"
         )
-        print(f"income={income}")
+
+        if river_basin.num_scenarios == 1:
+            assert round(income_calc, 4) == round(
+                income, 4
+            ), f"Income after decision {i} should be {income_calc} but it is {income}"
+        else:
+            for (
+                    income_calc_scenario,
+                    income_scenario
+            ) in zip(
+                income_calc, income
+            ):
+                assert round(income_calc_scenario, 4) == round(
+                    income_scenario, 4
+                ), f"Income after decision {i} should be {income_calc_scenario} but it is {income_scenario}"
 
         if check_volumes:
 
@@ -33,8 +49,8 @@ def test_river_basin(
             dam1_vol = np.clip(
                 old_state["dam1"]["vol"]
                 + (
-                    old_state["incoming_flow"]
-                    + old_state["dam1"]["unregulated_flow"]
+                    old_state["next_incoming_flow"]
+                    + old_state["dam1"]["next_unregulated_flow"]
                     - state["dam1"]["lags"][:, 0]
                 )
                 * river_basin.instance.get_time_step(),
@@ -44,8 +60,8 @@ def test_river_basin(
             # print(
             #     "dam1 calc in TEST",
             #     old_state["dam1"]["vol"],
-            #     old_state["incoming_flow"],
-            #     old_state["dam1"]["unregulated_flow"],
+            #     old_state["next_incoming_flow"],
+            #     old_state["dam1"]["next_unregulated_flow"],
             #     state["dam1"]["lags"][:, 0],
             #     dam1_vol,
             # )
@@ -54,8 +70,8 @@ def test_river_basin(
             dam2_vol = np.clip(
                 old_state["dam2"]["vol"]
                 + (
-                    old_state["dam1"]["turbined_flow"]
-                    + old_state["dam2"]["unregulated_flow"]
+                    state["dam1"]["turbined_flow"]
+                    + old_state["dam2"]["next_unregulated_flow"]
                     - state["dam2"]["lags"][:, 0]
                 )
                 * river_basin.instance.get_time_step(),
@@ -65,13 +81,14 @@ def test_river_basin(
             # print(
             #     "dam2 calc in TEST",
             #     old_state["dam2"]["vol"],
-            #     old_state["incoming_flow"],
-            #     old_state["dam2"]["unregulated_flow"],
+            #     state["dam1"]["turbined_flow"],
+            #     old_state["dam2"]["next_unregulated_flow"],
             #     state["dam2"]["lags"][:, 0],
             #     dam2_vol,
             # )
             if river_basin.num_scenarios == 1:
                 dam2_vol = dam2_vol.item()
+
             if river_basin.num_scenarios == 1:
                 assert round(state["dam1"]["vol"], 4) == round(
                     dam1_vol, 4
@@ -112,7 +129,7 @@ if __name__ == "__main__":
         paths_power_models=paths_power_models,
         num_scenarios=1,
     )
-    decisionsA = [[6.79, 6.58], [7.49, 6.73]]
+    decisionsA = [[6.79, 6.58], [7.49, 6.73], [7.49, 6.73], [7.49, 6.73], [7.49, 6.73]]
     test_river_basin(
         river_basin1,
         decisions=decisionsA
@@ -121,7 +138,7 @@ if __name__ == "__main__":
     print("---- SCENARIO A, WITH DEEP UPDATE ----")
     river_basin1.reset()
     income = river_basin1.deep_update(decisionsA)
-    print("---- deep update ----")
+    print(">>>> deep update")
     print(f"state after deep update: {river_basin1.get_state()}")
     print(f"accumulated income: {income}")
 
@@ -130,7 +147,7 @@ if __name__ == "__main__":
     decisions_all_periods = [[0, 0] for _ in range(instance.get_num_time_steps())]
     print(f"number of time steps: {instance.get_num_time_steps()}")
     income = river_basin1.deep_update(decisions_all_periods)
-    print("---- deep update ----")
+    print(">>>> deep update")
     print(f"state after deep update: {river_basin1.get_state()}")
     print(f"accumulated income: {income}")
 
@@ -157,7 +174,7 @@ if __name__ == "__main__":
     print("---- SCENARIOS A and B, WITH DEEP UPDATE ----")
     river_basin2.reset()
     income = river_basin2.deep_update(decisionsAB)
-    print("---- deep update ----")
+    print(">>>> deep update")
     print(f"state after deep update: {river_basin2.get_state()}")
     print(f"accumulated income: {income}")
 

flowing_basin/tests/test_rl_environment.py
@@ -28,23 +28,16 @@ print("---- initial observation ----")
 obs = env1.get_observation()
 print("initial observation:", obs.tensor)
 print("initial observation (normalized):", obs.normalized)
-print("initial reward:", env1.get_reward())
 
 # First decision
-print("---- decision 1 ----")
-action = torch.tensor([6.79, 6.58])
-reward, next_obs, done = env1.step(action)
-print("reward:", reward)
-print("observation:", next_obs.tensor)
-print("done:", done)
-
-# Second decision
-print("---- decision 2 ----")
-action = torch.tensor([7.49, 6.73])
-reward, next_obs, done = env1.step(action)
-print("reward:", reward)
-print("observation:", next_obs.tensor)
-print("done:", done)
+decisions = [[6.79, 6.58], [7.49, 6.73], [7.49, 6.73], [7.49, 6.73], [7.49, 6.73]]
+for i, decision in enumerate(decisions):
+    print(f">>>> decision {i}")
+    action = torch.tensor(decision)
+    reward, next_obs, done = env1.step(action)
+    print("reward:", reward)
+    print("observation:", next_obs.tensor)
+    print("done:", done)
 
 # ENVIRONMENT 2 (WITH INSTANCE 2)
 instance2 = Instance.from_json("../data/input_example3.json")

flowing_basin/tools/channel.py
@@ -27,6 +27,8 @@ class Channel:
 
         # Maximum flow of channel (m3/s)
         self.flow_max = instance.get_max_flow_of_channel(self.idx)
+        if self.num_scenarios > 1:
+            self.flow_max = np.repeat(self.flow_max, self.num_scenarios)
 
         # Initial flow limit (m3/s)
         self.flow_limit = self.get_flow_limit(dam_vol)
@@ -35,8 +37,8 @@ class Channel:
             idx=self.idx,
             flows_over_time=self.flows_over_time,
             instance=instance,
-            num_scenarios=self.num_scenarios,
             paths_power_models=paths_power_models,
+            num_scenarios=self.num_scenarios,
         )
 
     def reset(self, dam_vol: float | np.ndarray, instance: Instance):
@@ -52,16 +54,16 @@ class Channel:
 
         """
         The flow the channel can carry is limited by the volume of the dam
-        :param dam_vol: Volume of preceding dam (m3)
-        :return: Flow limit (maximum flow for given volume) (m3/s)
+        :param dam_vol:
+         - Volume of the dam connected to the channel (m3)
+         - OR Array of shape num_scenarios containing the volume of every scenario (m3)
+        :return:
+         - Flow limit (maximum flow for given volume) (m3/s)
+         - OR Array of shape num_scenarios with the flow limit in every scenario (m3/s)
         """
 
         if self.limit_flow_points is None:
-            flow_limit = self.flow_max
-            if self.num_scenarios > 1:
-                flow_limit = np.repeat(flow_limit, self.num_scenarios)
-
-            return flow_limit
+            return self.flow_max
 
         # Interpolate volume to get flow
         flow_limit = np.interp(
@@ -69,6 +71,7 @@ class Channel:
             self.limit_flow_points["observed_vols"],
             self.limit_flow_points["observed_flows"]
         )
+
         # Make sure limit is below maximum flow
         flow_limit = np.clip(flow_limit, 0, self.flow_max)
         if self.num_scenarios == 1:

flowing_basin/tools/power_group.py
@@ -6,6 +6,12 @@ import numpy as np
 
 
 class PowerGroup:
+
+    POWER_FLOW_LINE_COEFS = {
+        "dam1": [2.99625925, 0.05524465],
+        "dam2": [1.32432353, 0.25964468],
+    }
+
     def __init__(
         self,
         idx: str,
@@ -23,12 +29,12 @@ class PowerGroup:
 
         # Power generated (MW) and turbined flow (m3/s)
         self.power = self.get_power(flows_over_time)
-        self.turbined_flow = self.get_turbined_flow(flows_over_time)
+        self.turbined_flow = self.get_turbined_flow(self.power)
 
     def reset(self, flows_over_time: np.ndarray):
 
         self.power = self.get_power(flows_over_time)
-        self.turbined_flow = self.get_turbined_flow(flows_over_time)
+        self.turbined_flow = self.get_turbined_flow(self.power)
 
     @staticmethod
     def get_power_model(path_power_model: str) -> lightgbm.LGBMClassifier:
@@ -70,19 +76,22 @@ class PowerGroup:
             power = power.item()
         return power
 
-    def get_turbined_flow(self, flows_over_time: np.ndarray) -> float | np.ndarray:
+    def get_turbined_flow(self, power: float | np.ndarray) -> float | np.ndarray:
 
         """
 
-        :param flows_over_time: Array of shape num_scenarios x num_lags with all past flows of the channel (m3/s)
+        :param power:
+         - Power (MW)
+         - OR Array of shape num_scenarios with power valoes (MW)
         :return:
-         - Turbined flow in this time step (m3/s)
-         - OR Array of shape num_scenarios containing the turbined flow of every scenario (m3/s)
+         - Corresponding turbined flow (m3/s)
+         - OR Array of shape num_scenarios with the corresponding turbined flows (m3/s)
         """
 
-        # TODO: implement this function
+        line = np.poly1d(self.POWER_FLOW_LINE_COEFS[self.idx])
+        turbined_flow = line(power)
 
-        return 0
+        return turbined_flow
 
     def update(self, flows_over_time: np.ndarray) -> float | np.ndarray:
 
@@ -95,7 +104,7 @@ class PowerGroup:
         """
 
         self.power = self.get_power(flows_over_time)
-        self.turbined_flow = self.get_turbined_flow(flows_over_time)
+        self.turbined_flow = self.get_turbined_flow(self.power)
 
         # Bring turbined flow upstream, since it is used to update the volume of the next dam
         return self.turbined_flow
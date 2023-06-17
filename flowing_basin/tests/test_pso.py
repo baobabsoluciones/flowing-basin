7fcb949a4513a1ff00e3ccda25b52eb38c97ced3
RodrigoCastroF <79080278+RodrigoCastroF@users.noreply.github.com>
2023-06-17 21:26:46 +0200

    Cleaned some files + tested PSO w/ instance3 using k=0 (inconsistent results)

--- flowing_basin/tests/test_pso.py
+++ flowing_basin/tests/test_pso.py
@@ -3,7 +3,7 @@ from flowing_basin.solvers import PSOConfiguration, PSO
 from datetime import datetime
 import os
 
-EXAMPLE = 1
+EXAMPLE = 3
 NUM_DAMS = 6
 NUM_DAYS = 1
 
@@ -38,17 +38,17 @@ pso = PSO(
 
 # Optimal solution found by PSO ---- #
 
-# path_parent = "../data"
-# dir_name = f"output_instance{EXAMPLE}_PSO_{NUM_DAMS}dams_{NUM_DAYS}days_{datetime.now().strftime('%Y-%m-%d %H.%M')}_mode={pso.config.mode}_k={pso.config.flow_smoothing}"
-# options = {'c1': 2.905405139888455, 'c2': 0.4232260541405988, 'w': 0.4424113459034113}
-# status = pso.solve(options, num_particles=200, num_iters=100_000, timeout=8*60)
-# print("status:", status)
-# print("solver info:", pso.solver_info)
-
-sol_lp = Solution.from_json(f"../data/output_instance{EXAMPLE}_LPmodel_V2_{NUM_DAMS}dams_{NUM_DAYS}days.json")
 path_parent = "../data"
-dir_name = f"output_instance{EXAMPLE}_LPmodel_V2_{NUM_DAMS}dams_{NUM_DAYS}days"
-pso.solution = sol_lp
+dir_name = f"output_instance{EXAMPLE}_PSO_{NUM_DAMS}dams_{NUM_DAYS}days_{datetime.now().strftime('%Y-%m-%d %H.%M')}_mode={pso.config.mode}_k={pso.config.flow_smoothing}"
+options = {'c1': 2.905405139888455, 'c2': 0.4232260541405988, 'w': 0.4424113459034113}
+status = pso.solve(options, num_particles=200, num_iters=100_000, timeout=8*60)
+print("status:", status)
+print("solver info:", pso.solver_info)
+
+# sol_lp = Solution.from_json(f"../data/output_instance{EXAMPLE}_LPmodel_V2_{NUM_DAMS}dams_{NUM_DAYS}days.json")
+# path_parent = "../data"
+# dir_name = f"output_instance{EXAMPLE}_LPmodel_V2_{NUM_DAMS}dams_{NUM_DAYS}days"
+# pso.solution = sol_lp
 
 sol_inconsistencies = pso.solution.check()
 if sol_inconsistencies:
@@ -59,9 +59,6 @@ pso.river_basin.deep_update(pso.solution.to_flows(), is_relvars=False)
 print("optimal solution's objective function values:", pso.objective_function_values_env())
 print("optimal solution's full objective function value:", pso.objective_function_env())
 print("optimal solution's full objective function value (cornflow method):", pso.get_objective())
-dam1_income = pso.river_basin.history["dam1_income"].loc[0:96].sum()
-dam2_income = pso.river_basin.history["dam2_income"].loc[0:98].sum()
-print("optimal solution's income (from history):", dam1_income + dam2_income)
 print(pso.river_basin.history.to_string())
 pso.save_solution_info(path_parent=path_parent, dir_name=dir_name)
 pso.river_basin.history.to_excel(os.path.join(path_parent, dir_name, "history.xlsx"))

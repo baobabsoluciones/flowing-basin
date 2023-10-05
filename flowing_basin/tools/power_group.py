Fixed bug when startup flow = shutdown flow (and simplified num_active_groups calculation)

flowing_basin/tools/power_group.py
@@ -33,12 +33,12 @@ class PowerGroup:
             self.power_model = self.get_nonlinear_power_model(paths_power_models[self.idx])
         self.relevant_lags = instance.get_relevant_lags_of_dam(self.idx)
         self.verification_lags = instance.get_verification_lags_of_dam(self.idx)
-        self.turbined_flow_points = instance.get_turbined_flow_obs_for_power_group(
-            self.idx
-        )
-        self.startup_flows = instance.get_startup_flows_of_power_group(self.idx)
-        self.shutdown_flows = instance.get_shutdown_flows_of_power_group(self.idx)
+        self.turbined_flow_points = instance.get_turbined_flow_obs_for_power_group(self.idx)
         self.time_step_hours = instance.get_time_step_seconds() / 3600
+
+        startup_flows = instance.get_startup_flows_of_power_group(self.idx)
+        shutdown_flows = instance.get_shutdown_flows_of_power_group(self.idx)
+        self.turbined_bins, self.turbined_bin_groups = self.get_turbined_bins_and_groups(startup_flows, shutdown_flows)
 
         # Save the decision horizon and this power group's impact horizon
         self.decision_horizon = instance.get_decision_horizon()
@@ -124,6 +124,43 @@ class PowerGroup:
             model_load = pickle.load(open(path_power_model, "rb"))
 
         return model_load
+
+    @staticmethod
+    def get_turbined_bins_and_groups(
+            startup_flows: list[float], shutdown_flows: list[float], epsilon: float = 1e-4
+    ) -> tuple[np.ndarray, np.ndarray]:
+
+        """
+        Get the first flow of each turbined flow bin (zone),
+        and the corresponding number of active power groups.
+        """
+
+        num_active_groups = 0
+        turbined_bins = []
+        turbined_bin_groups = [num_active_groups]
+
+        for shutdown_flow, startup_flow in zip(shutdown_flows, startup_flows):
+
+            if shutdown_flow != startup_flow:
+                # Limit zone: turbined_flow in open interval (shutdown flow, startup_flow)
+                turbined_bins.append(shutdown_flow + epsilon)
+                turbined_bin_groups.append(num_active_groups + .5)
+
+                # Next number of power groups: turbined_flow in close interval [starup_flow, next_shutdown_flow]
+                turbined_bins.append(startup_flow - epsilon)
+                turbined_bin_groups.append(num_active_groups + 1)
+
+            else:
+                # We consider there is no limit zone
+                turbined_bins.append(startup_flow - epsilon)
+                turbined_bin_groups.append(num_active_groups + 1)
+
+            num_active_groups += 1
+
+        turbined_bins = np.array(turbined_bins)
+        turbined_bin_groups = np.array(turbined_bin_groups)
+
+        return turbined_bins, turbined_bin_groups
 
     def update_power_turbined_flow_nonlinear(self, past_flows: np.ndarray):
 
@@ -228,38 +265,8 @@ class PowerGroup:
         ...
         """
 
-        # Obtain the number of exceeded startup flows for every scenario
-        # - Turn the FLOW 1D array into a 2D array, repeating the 1D array in as many ROWS as there are STARTUP FLOWS
-        # - Turn the STARTUP FLOWS 1D array into a 2D array, repeating the 1D array in as many COLS as there are FLOWS
-        # - Comparing both 2D arrays, determine, for each flow, the startup flows are exceeded, and sum them
-        # We subtract epsilon in the comparison so, if the turbined flow happens to equal the startup flow,
-        # the turbined flow is NOT considered to be in a limit zone, but in the higher number of active power groups
-        flow_broadcast_to_startup_flows = np.tile(
-            turbined_flow, (len(self.startup_flows), 1)
-        )
-        startup_flows_broadcast_to_flows = np.transpose(
-            np.tile(self.startup_flows, (len(turbined_flow), 1))
-        )
-        exceeded_startup_flows = np.sum(
-            flow_broadcast_to_startup_flows > startup_flows_broadcast_to_flows - epsilon, axis=0
-        )
-
-        # Obtain the number of exceeded shutdown flows for every scenario
-        # This will only be different for flows in between a startup and shutdown flow
-        # We add epsilon in the comparison so, if the turbined flow happens to equal the shutdown flow,
-        # the turbined flow is NOT considered to be in a limit zone, but in the lower number of active power groups
-        flow_broadcast_to_shutdown_flows = np.tile(
-            turbined_flow, (len(self.shutdown_flows), 1)
-        )
-        shutdown_flows_broadcast_to_flows = np.transpose(
-            np.tile(self.shutdown_flows, (len(turbined_flow), 1))
-        )
-        exceeded_shutdown_flows = np.sum(
-            flow_broadcast_to_shutdown_flows > shutdown_flows_broadcast_to_flows + epsilon, axis=0
-        )
-
-        # Get the average of the number of exceeded startup flows and the number of exceeded shutdown flows
-        num_active_power_groups = (exceeded_startup_flows + exceeded_shutdown_flows) / 2
+        bin_indices = np.digitize(turbined_flow, self.turbined_bins)
+        num_active_power_groups = self.turbined_bin_groups[bin_indices]
 
         return num_active_power_groups
 
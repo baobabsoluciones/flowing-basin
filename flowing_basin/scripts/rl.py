8e9157f60843a13616c0f17006afc52f6116cb3c
RodrigoCastroF <79080278+RodrigoCastroF@users.noreply.github.com>
2024-01-03 19:22:19 +0800

    Prepared experimentation

--- flowing_basin/scripts/rl.py
+++ flowing_basin/scripts/rl.py
@@ -1,18 +1,10 @@
 from flowing_basin.solvers.rl import ReinforcementLearning
 
-# for obs in ["O2", "O22", "O221"]:
-#     rl = ReinforcementLearning(f"A1G0{obs}R1T02", verbose=2)
-#     rl.collect_obs()
-#     # rl.plot_histograms_projector()
-#     rl.train()
-#     # rl.plot_histograms_observations()
-#     # rl.plot_training_curve()
-
-# rl = ReinforcementLearning("A1G0O2R1T02", verbose=2)
-# rl.plot_histograms_observations()
-# rl.plot_training_curve()
-# rl.plot_training_curves_compare(["rl-A1G0O22R1T02", "rl-A1G0O221R1T02"], ["MILP"], values=["income", "acc_reward"])
-
-rl = ReinforcementLearning("A1G0O12R1T2", verbose=2)
+rl = ReinforcementLearning("A1G0O2R1T02", verbose=2)
 rl.collect_obs()
+rl.plot_histograms_projector()
+rl.train()
+rl.plot_histograms_observations()
+rl.plot_training_curve()
+rl.plot_training_curves_compare(["rl-A1G0O22R1T02", "rl-A1G0O221R1T02"], ["MILP"], values=["income", "acc_reward"])
 
---
+++ flowing_basin/scripts/rl_experiments.py
@@ -0,0 +1,14 @@
+from flowing_basin.solvers.rl import ReinforcementLearning
+from itertools import product
+
+ACTION = ["A1"]
+GENERAL = ["G0", "G1"]
+OBSERVATION = ["O1", "O12", "O121", "O2", "O22", "O221"]
+REWARD = ["R1"]
+# TRAINING = ["T1", "T2"]
+TRAINING = ["T12", "T22"]
+
+for action, general, obs, reward, training in product(ACTION, GENERAL, OBSERVATION, REWARD, TRAINING):
+    rl = ReinforcementLearning(f"{action}{general}{obs}{reward}{training}", verbose=2)
+    rl.collect_obs()
+    rl.train()

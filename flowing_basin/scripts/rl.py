b8daa090408f8f210d4b04013d622b86d585bafc
RodrigoCastroF <79080278+RodrigoCastroF@users.noreply.github.com>
2024-01-04 22:57:52 +0800

    Fixed G1 LPmodel baselines

--- flowing_basin/scripts/rl.py
+++ flowing_basin/scripts/rl.py
@@ -14,6 +14,5 @@ from flowing_basin.solvers.rl import ReinforcementLearning
 # rl.plot_training_curve()
 # rl.plot_training_curves_compare(["rl-A1G0O22R1T02", "rl-A1G0O221R1T02"], ["MILP"], values=["income", "acc_reward"])
 
-# print(ReinforcementLearning.get_all_agents(".*G0.*T1"))
-ReinforcementLearning.plot_all_training_curves(".*G0.*T1")
+ReinforcementLearning.plot_all_training_curves(".*G1.*T2")
 

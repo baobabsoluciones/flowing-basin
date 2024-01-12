b1e68fc5434ed672be9b9f462ae849cf15ffdfc1
Rodrigo Castro Freibott <79080278+RodrigoCastroF@users.noreply.github.com>
2024-01-12 14:47:08 +0100

    RL Heurisic baseline for G0 (cont.)

--- flowing_basin/tests/test_heuristic_rl_baseline.py
+++ flowing_basin/tests/test_heuristic_rl_baseline.py
@@ -7,19 +7,19 @@ EXAMPLES = [f"Percentile{percentile:02}" for percentile in range(0, 110, 10)]
 for example in EXAMPLES:
 
     path_instance = f"../instances/instances_base/instance{example}.json"
-    path_sol = f"../solutions/rl_baselines/instance{example}_Heuristic_k=0_NoPowerPenalties.json"
+    path_sol = f"../solutions/rl_baselines/instance{example}_Heuristic_k=2_PowerPenalties.json"
 
     instance = Instance.from_json(path_instance)
     config = HeuristicConfiguration(
         mode="linear",
         volume_shortage_penalty=0,
         volume_exceedance_bonus=0,
-        startups_penalty=0.,
-        limit_zones_penalty=0.,
+        startups_penalty=50.,
+        limit_zones_penalty=50.,
         volume_objectives={
             dam_id: instance.get_historical_final_vol_of_dam(dam_id) for dam_id in instance.get_ids_of_dams()
         },
-        flow_smoothing=0,
+        flow_smoothing=2,
     )
 
     # Putting greedy=True is redundant, since random bias is False by default, but just to make sure...

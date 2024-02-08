865c9ca18dfbf459a23cf19356498daca3611277
RodrigoCastroF <79080278+RodrigoCastroF@users.noreply.github.com>
2024-02-08 14:59:24 +0100

    Ensured MILP consistency

--- flowing_basin/tests/test_consistency.py
+++ flowing_basin/tests/test_consistency.py
@@ -15,7 +15,7 @@ class TestBaselinesConsistency(TestCase):
         solvers = {sol.get_solver() for sol in solutions}
         self.solutions = {solver: [sol for sol in solutions if sol.get_solver() == solver] for solver in solvers}
 
-    def check_consistency(self, solution: Solution):
+    def check_consistency(self, solution: Solution, epsilon: float = 0.005):
 
         """
         Check that the objective function values of each baseline
@@ -34,6 +34,16 @@ class TestBaselinesConsistency(TestCase):
         config = solution.get_configuration()
         instance = Instance.from_name(solution.get_instance_name())
         mode = getattr(config, 'mode', 'linear')
+        detail_penalty = dict(
+            startups=config.startups_penalty,
+            limit_zones=config.limit_zones_penalty,
+            volume_shortage_m3=config.volume_shortage_penalty,
+            volume_exceedance_m3=config.volume_exceedance_bonus
+        )
+
+        # By default, force the objective function to match 2 decimal places (see the end of this function)
+        delta_obj_fun = None
+        places_obj_fun = 2
 
         # Do not smooth flows. Flow smoothing compliance should be checked separately
         river_basin = RiverBasin(
@@ -49,36 +59,62 @@ class TestBaselinesConsistency(TestCase):
 
         # Check that the details from the simulator are the same as the details from the solution
         for dam_id in instance.get_ids_of_dams():
+
             sol_details = solution.get_objective_details(dam_id)
-            if sol_details is None:
-                warnings.warn(
-                    f"The solution of {solution.get_solver()} with config {solution.get_configuration().to_dict()} "
+            self.assertIsNotNone(
+                sol_details,
+                msg=f"The solution of {solution.get_solver()} with config {solution.get_configuration().to_dict()} "
                     f"for instance {solution.get_instance_name()} "
                     f"does not have objective function details."
-                )
-            else:
-                sim_details = river_basin.get_objective_function_details(dam_id, config=config)
-                sim_details = {detail_key: detail_value.item() for detail_key, detail_value in sim_details.items()}
-                for detail, value in sim_details.items():
-                    self.assertAlmostEqual(
-                        sol_details[detail],
-                        value,
-                        places=2,
-                        msg=f"The solution of {solution.get_solver()} with config {solution.get_configuration().to_dict()} "
+            )
+
+            sim_details = river_basin.get_objective_function_details(dam_id, config=config)
+            sim_details = {detail_key: detail_value.item() for detail_key, detail_value in sim_details.items()}
+
+            for detail, value in sim_details.items():
+
+                # Skip those details with no penalty; they are not required to be consistent
+                if detail in detail_penalty:
+                    if detail_penalty[detail] == 0.:
+                        continue
+
+                # Exceptionally, allow a variation of 1 unit of the startups of dam1
+                # This is an edge case: since there is a startup flow that matches a shutdown flow in dam1,
+                # the MILP can _choose_ whether it is in a limit zone or in a startup depending on what it is better
+                # In this case, the objective function can also differ in the value of the penalty
+                if dam_id == "dam1" and detail == "startups":
+                    if abs(sol_details[detail] - value) == 1:
+                        warnings.warn(
+                            f"Edge case with {solution.get_solver()} in {solution.get_instance_name()}: "
+                            f"The solution of {solution.get_solver()} with config {solution.get_configuration().to_dict()} "
                             f"for instance {solution.get_instance_name()} "
-                            f"has a {detail} with value {sol_details[detail]}, "
-                            f"but the simulator predicts the value {value}."
-                    )
+                            f"has a {detail} with value {sol_details[detail]} for dam {dam_id}, "
+                            f"but the simulator predicts the value {value}. "
+                        )
+                        delta_obj_fun = config.startups_penalty + epsilon
+                        places_obj_fun = None
+                        continue
+
+                self.assertAlmostEqual(
+                    sol_details[detail],
+                    value,
+                    delta=delta_obj_fun,
+                    places=places_obj_fun,
+                    msg=f"The solution of {solution.get_solver()} with config {solution.get_configuration().to_dict()} "
+                        f"for instance {solution.get_instance_name()} "
+                        f"has a {detail} with value {sol_details[detail]} for dam {dam_id}, "
+                        f"but the simulator predicts the value {value}."
+                )
 
         # Check global objective function value
         # This test is redundant if details are provided
-        # TODO: enforce details in all solvers and remove these lines of code
         sol_obj_fun = solution.get_objective_function()
         sim_obj_fun = river_basin.get_objective_function_value(config=config).item()
         self.assertAlmostEqual(
             sol_obj_fun,
             sim_obj_fun,
-            places=2,
+            places=places_obj_fun,
+            delta=delta_obj_fun,
             msg=f"The solution of {solution.get_solver()} with config {solution.get_configuration().to_dict()} "
                 f"for instance {solution.get_instance_name()} "
                 f"has an objective function value of {sol_obj_fun}, "

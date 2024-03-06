29ee47ab79d759cdda2372e807ea842307a12d17
Rodrigo Castro Freibott <79080278+RodrigoCastroF@users.noreply.github.com>
2024-03-06 01:34:48 +0100

    Prepared Experiment 8

---
+++ flowing_basin/scripts/rl_experiment8.py
@@ -0,0 +1,26 @@
+"""
+RL Experiment 8
+This script trains agents with reward adjusted according to MILP estimated performance
+and/or with rl-greedy's reference recomputed for every period
+"""
+
+from flowing_basin.solvers.rl import ReinforcementLearning
+from itertools import product
+
+actions = ["A1", "A113"]
+generals = ["G0", "G1"]
+observations = ["O231", "O232"]
+
+for action, general, observation in product(actions, generals, observations):
+
+    # Recomputing rl-greedy's average reward only makes a difference when there is more than one step per episode
+    rewards = ["R221", "R222", "R223", "R23", "R231", "R232", "R233"] if action == "A1" else ["R23"]
+
+    for reward in rewards:
+
+        training = "T13" if action == "A1" else "T33"
+
+        rl = ReinforcementLearning(f"{action}{general}{observation}{reward}{training}", verbose=2)
+        rl.train(save_agent=False)
+
+# Note we will train 2(G) * 2(O) * 7(R) + 2(G) * 2(O) = 28 + 4 = 32 agents

0b68c0903de479a2b59e43b2c32221e5e802ae23
RodrigoCastroF <79080278+RodrigoCastroF@users.noreply.github.com>
2024-05-11 21:26:56 +0200

    config definition in Baseline class

--- flowing_basin/scripts/rl_baseline.py
+++ flowing_basin/scripts/rl_baseline.py
@@ -5,4 +5,11 @@ TODO: once the Baseline class is finished, substitute lp_rl_baseline.py and heur
 
 from flowing_basin.solvers import Baseline
 
-print(Baseline.get_general_config('G0'))
+print(Baseline(general_config='G0', solver='MILP').config)
+print(Baseline(general_config='G0', solver='PSO').config)
+print(Baseline(general_config='G0', solver='PSO-RBO').config)
+print(Baseline(general_config='G0', solver='Heuristic').config)
+print(Baseline(general_config='G1', solver='MILP').config)
+print(Baseline(general_config='G1', solver='PSO').config)
+print(Baseline(general_config='G1', solver='PSO-RBO').config)
+print(Baseline(general_config='G1', solver='Heuristic').config)
--- flowing_basin/solvers/baseline.py
+++ flowing_basin/solvers/baseline.py
@@ -1,4 +1,5 @@
 import os
+from cornflow_client.core.tools import load_json
 from flowing_basin.solvers.rl import GeneralConfiguration
 from flowing_basin.solvers import (
     Heuristic, HeuristicConfiguration, LPModel, LPConfiguration, PSO, PSOConfiguration,
@@ -18,18 +19,23 @@ class Baseline:
         "PSO": (PSO, PSOConfiguration),
         "PSO-RBO": (PsoRbo, PsoRboConfiguration)
     }
+    hyperparams_folder = os.path.join(os.path.dirname(__file__), "../hyperparams")
     config_info = (os.path.join(os.path.dirname(__file__), "../rl_data/configs/general"), GeneralConfiguration)
     baselines_folder = os.path.join(os.path.dirname(__file__), "../rl_data/baselines")
     instance_names = [f"Percentile{percentile:02}" for percentile in range(0, 110, 10)]
 
     def __init__(self, general_config: str, solver: str):
 
-        general_config = self.get_general_config(general_config)
-        self.solver_class, self.config_class = Baseline.solver_classes[solver]
-        # TODO: create a self.config attribute using self.config_class with general_config and other values
+        self.solver_class, config_class = Baseline.solver_classes[solver]
+
+        solver_config_path = os.path.join(Baseline.hyperparams_folder, f"{solver.lower()}.json")
+        solver_config_dict = load_json(solver_config_path)
+        general_config_dict = Baseline.get_general_config_dict(general_config)
+        Baseline.update_config(solver_config_dict, general_config_dict)
+        self.config = config_class.from_dict(solver_config_dict)
 
     @staticmethod
-    def get_general_config(general_config: str) -> GeneralConfiguration:
+    def get_general_config_dict(general_config: str) -> dict:
 
         """
         Get the GeneralConfiguration object from the configuration string (e.g., "G0")
@@ -37,5 +43,17 @@ class Baseline:
 
         config_folder, config_class = Baseline.config_info
         config_path = os.path.join(config_folder, general_config + ".json")
-        config = config_class.from_json(config_path)
-        return config
+        config_dict = load_json(config_path)
+        return config_dict
+
+    @staticmethod
+    def update_config(solver_config_dict: dict, general_config_dict: dict):
+        """
+        Sets the attributes of `solver_config_dict` with missing values to
+        the values of these attributes in `general_config_dict`.
+        Assumes the attributes with missing values in `solver_config_dict` are all present in `general_config_dict`.
+        Only the attributes with missing values in `solver_config_dict` are changed.
+        """
+        for key, value in solver_config_dict.items():
+            if value is None:
+                solver_config_dict[key] = general_config_dict[key]

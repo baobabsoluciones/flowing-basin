9f161e794be2df7e7163ad21de427c37eca60dd9
Rodrigo Castro Freibott <79080278+RodrigoCastroF@users.noreply.github.com>
2024-05-16 00:36:40 +0200

    Calculated MILP G0 G1 sols with new PC

--- flowing_basin/scripts/rl_baseline.py
+++ flowing_basin/scripts/rl_baseline.py
@@ -5,7 +5,8 @@ Script to compute solutions using PSO, MILP, etc. as baselines for RL
 
 from flowing_basin.solvers import Baseline
 
-# Baseline(general_config='G0', solver='MILP').solve(filename_tail="newpc")
+for config in ['G0', 'G1']:
+    Baseline(general_config=config, solver='MILP').solve()
 
 # Baseline(general_config='G1', solver='PSO', max_time=5).tune(
 #     num_trials=2, instance_names=['Percentile25'], num_replications=1
@@ -14,6 +15,6 @@ from flowing_basin.solvers import Baseline
 #     instance_names=['Percentile25'],
 # )
 
-Baseline(general_config='G1', solver='PSO', max_time=30).solve(
-    instance_names=['Percentile25'], num_replications=1
-)
+# Baseline(general_config='G1', solver='PSO', max_time=30).solve(
+#     instance_names=['Percentile25'], num_replications=1
+# )

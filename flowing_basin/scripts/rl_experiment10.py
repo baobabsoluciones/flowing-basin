0a2a47502ce231c1db81a976a854702e5bee67c8
RodrigoCastroF <79080278+RodrigoCastroF@users.noreply.github.com>
2024-05-16 22:25:47 +0200

    Simplified Experiment 10

--- flowing_basin/scripts/rl_experiment10.py
+++ flowing_basin/scripts/rl_experiment10.py
@@ -7,27 +7,15 @@ from flowing_basin.solvers.rl import ReinforcementLearning
 from itertools import product
 
 generals = ["G0", "G1"]
-actions = ["A1", "A31", "A32", "A33", "A313", "A323", "A333"]
-multi_block = {"A313", "A323", "A333"}
+actions = ["A31", "A32", "A33"]
+rewards = ["R1", "R22"]
+training = "T1002"  # Testing: T1302
+observation = "O231"
 
-for action, general in product(actions, generals):
+for action, general, reward in product(actions, generals, rewards):
 
-    # Train with A2C and PPO
-    # Multi-action agents have a larger network
-    # Also, given their high training time, multi-action agents are only trained with reward R22
-    if action in multi_block:
-        trainings = ["T1071", "T1072"]
-        observation = "O2"
-        rewards = ["R22"]
-    else:
-        trainings = ["T1001", "T1002"]
-        observation = "O231"
-        rewards = ["R1", "R22"]
+    rl = ReinforcementLearning(f"{action}{general}{observation}{reward}{training}", verbose=2)
+    rl.train()
 
-    for training, reward in product(trainings, rewards):
-        rl = ReinforcementLearning(f"{action}{general}{observation}{reward}{training}", verbose=2)
-        rl.train()
-
-# Note we will train 4(A) * 2(G) * 1(O) * 2(R) * 2(T) = 32 agents of single-action blocks,
-# and 3(A) * 2(G) * 1(O) * 1(R) * 2(T) = 12 agents of multi-action blocks
-# Approximate training time: 32 agents * 0.5 hours/agent + 12 agents * 6.5 hours/agent = 94 hours = 4 days
+# Note we will train 2(G) * 3(A) * 2(R) * 1(O) * 1(T) = 12 agents of single-action blocks,
+# Approximate training time: 12 agents * 0.5 hours/agent = 6 hours

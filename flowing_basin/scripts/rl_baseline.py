3667462f5dabb47b1307405519da6c0f5c37cd43
RodrigoCastroF <79080278+RodrigoCastroF@users.noreply.github.com>
2024-05-16 13:23:01 +0200

    Prepared first PSO experimentation

--- flowing_basin/scripts/rl_baseline.py
+++ flowing_basin/scripts/rl_baseline.py
@@ -5,16 +5,13 @@ Script to compute solutions using PSO, MILP, etc. as baselines for RL
 
 from flowing_basin.solvers import Baseline
 
-for config in ['G2', 'G3']:
-    Baseline(general_config=config, solver='MILP').solve()
+configs = ['G0', 'G1', 'G2', 'G3']
+for config in configs:
+    Baseline(general_config=config, solver='PSO').solve(num_replications=5)
 
 # Baseline(general_config='G1', solver='PSO', max_time=5).tune(
 #     num_trials=2, instance_names=['Percentile25'], num_replications=1
 # )
 # Baseline(general_config='G1', solver='PSO', max_time=5, tuned_hyperparams=True).solve(
 #     instance_names=['Percentile25'],
-# )
-
-# Baseline(general_config='G1', solver='PSO', max_time=30).solve(
-#     instance_names=['Percentile25'], num_replications=1
 # )

4b68bc6159be8d6b0bf7c2778e76d034cecf19bc
RodrigoCastroF <79080278+RodrigoCastroF@users.noreply.github.com>
2024-05-18 18:09:50 +0200

    Refactored rl_baselines.py for reusability

--- flowing_basin/scripts/rl_baselines.py
+++ flowing_basin/scripts/rl_baselines.py
@@ -6,12 +6,19 @@ Script to analyze existing solutions that act as baselines for RL
 from flowing_basin.solvers import Baselines
 import matplotlib.pyplot as plt
 
-# Baselines(solvers=['MILP', 'PSO'], general_config='G0').barchart_instances()
 
-fig, axes = plt.subplots(2, 2, figsize=(12, 12))
-axes = axes.flatten()
-for i, ax in enumerate(axes):
-    Baselines(solvers=['MILP', 'PSO'], general_config=f'G{i}').barchart_instances_ax(ax)
-plt.tight_layout()
-plt.savefig("reports/barchart_instances_MILP_PSO.png")
-plt.show()
+def barchart_instances(solvers: list[str]):
+
+    fig, axes = plt.subplots(2, 2, figsize=(12, 12))
+    axes = axes.flatten()
+    for i, ax in enumerate(axes):
+        Baselines(solvers=solvers, general_config=f'G{i}').barchart_instances_ax(ax)
+    plt.tight_layout()
+    solvers_title = "_".join(solvers)
+    plt.savefig(f"reports/barchart_instances_{solvers_title}.png")
+    plt.show()
+
+
+if __name__ == "__main__":
+
+    barchart_instances(['MILP', 'PSO'])

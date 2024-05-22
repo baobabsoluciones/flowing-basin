ef60a69e12e703b62ffca78eedd3879638ab345e
Rodrigo Castro Freibott <79080278+RodrigoCastroF@users.noreply.github.com>
2024-05-22 23:50:03 +0200

    Finished PSO tuning in G1

--- flowing_basin/scripts/baseline.py
+++ flowing_basin/scripts/baseline.py
@@ -9,12 +9,12 @@ from flowing_basin.solvers import Baseline
 # for config in configs:
 #     Baseline(general_config=config, solver='PSO').solve(num_replications=5)
 
-# Baseline(general_config='G0', solver='PSO').tune(num_trials=100, num_replications=1)
-
-Baseline(general_config='G0', solver='PSO', max_time=5, tuned_hyperparams=True).solve(
-    instance_names=['Percentile25'],
-)
+Baseline(general_config='G1', solver='PSO').tune(num_trials=100, num_replications=1)
 
 # Baseline(general_config='G1', solver='PSO-RBO', max_time=30).tune(
 #     num_trials=4, instance_names=['Percentile25'], num_replications=1
+# )
+
+# Baseline(general_config='G1', solver='PSO', max_time=5, tuned_hyperparams=True).solve(
+#     instance_names=['Percentile25'],
 # )

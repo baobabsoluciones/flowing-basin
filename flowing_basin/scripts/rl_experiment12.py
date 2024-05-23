1d575c7c511c63ded0ab1ca2ecdf86a658a35df8
RodrigoCastroF <79080278+RodrigoCastroF@users.noreply.github.com>
2024-05-23 19:51:02 +0200

    Added more agents in Experiment 12

--- flowing_basin/scripts/rl_experiment12.py
+++ flowing_basin/scripts/rl_experiment12.py
@@ -5,6 +5,7 @@ This script trains the best agents of previous experiments using 6 dams instead 
 The best agents are:
 - For G0: rl-A31G0O231R1T1002 from Experiment 10 (+4.25% above rl-greedy) & rl-A1G0O231R22T1 from Experiment 7 (+3.01%).
 - For G1: rl-A113G1O2R22T302 from Experiment 9 (+1.54% above rl-greedy) & rl-A113G1O232R22T3 from Experiment 7 (+1.53%).
+However, we will also try the agents rl-A31G0O231R1T1002 and rl-A1G0O231R22T1 in G1.
 
 To train these agents for 6 dams:
 - G0 and G1 will be replaced by G2 and G3, respectively.
@@ -19,7 +20,9 @@ from flowing_basin.solvers.rl import ReinforcementLearning
 
 best_agents = [
     "rl-A31G2O231R1T1402",
+    "rl-A31G3O231R1T1402",
     "rl-A1G2O231R22T14",
+    "rl-A1G3O231R22T14",
     "rl-A113G3O2R22T342",
     "rl-A113G3O2R22T349",
     "rl-A113G3O232R22T34",

8d364516742382e764f2ecfebe31a4cb07804bc3
Rodrigo Castro Freibott <79080278+RodrigoCastroF@users.noreply.github.com>
2024-06-04 19:59:14 +0200

    Added PSO-RBO sols (not tuned)

--- flowing_basin/scripts/baseline.py
+++ flowing_basin/scripts/baseline.py
@@ -9,7 +9,7 @@ GENERAL_CONFIGS = ['G0', 'G01', 'G1', 'G2', 'G21', 'G3']
 
 for config in GENERAL_CONFIGS:
     # Baseline(general_config=config, solver='PSO-RBO', max_time=30).solve(instance_names=['Percentile25'])
-    Baseline(general_config=config, solver='PSO-RBO').solve()
+    Baseline(general_config=config, solver='PSO-RBO').solve(num_replications=5)
 
 # for config in GENERAL_CONFIGS:
 #     Baseline(general_config=config, solver='PSO-RBO', max_time=5).tune(num_trials=4, num_replications=1, instance_names=['Percentile25'])

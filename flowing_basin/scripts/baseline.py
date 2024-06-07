f5ac7a217dfa945fbe13ce2b5a88918749efa8a4
Rodrigo Castro Freibott <79080278+RodrigoCastroF@users.noreply.github.com>
2024-06-07 13:21:04 +0200

    Finished PSO-RBO tuning in G2, G21 and G3

--- flowing_basin/scripts/baseline.py
+++ flowing_basin/scripts/baseline.py
@@ -5,7 +5,8 @@ Script to compute solutions using PSO, MILP, etc. as baselines for RL
 
 from flowing_basin.solvers import Baseline
 
-GENERAL_CONFIGS = ['G0', 'G01', 'G1', 'G2', 'G21', 'G3']
+# GENERAL_CONFIGS = ['G0', 'G01', 'G1', 'G2', 'G21', 'G3']
+GENERAL_CONFIGS = ['G2', 'G21', 'G3']
 
 # for config in GENERAL_CONFIGS:
 #     # Baseline(general_config=config, solver='PSO-RBO', max_time=30).solve(instance_names=['Percentile25'])

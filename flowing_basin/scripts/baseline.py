1b282177c78d55b5bc5c3abdddab652b701cc7fe
RodrigoCastroF <79080278+RodrigoCastroF@users.noreply.github.com>
2024-06-07 13:51:33 +0200

    Prepared tuned PSO-RBO and PSO sols (5 replications)

--- flowing_basin/scripts/baseline.py
+++ flowing_basin/scripts/baseline.py
@@ -5,12 +5,10 @@ Script to compute solutions using PSO, MILP, etc. as baselines for RL
 
 from flowing_basin.solvers import Baseline
 
-# GENERAL_CONFIGS = ['G0', 'G01', 'G1', 'G2', 'G21', 'G3']
-GENERAL_CONFIGS = ['G2', 'G21', 'G3']
-
-# for config in GENERAL_CONFIGS:
-#     # Baseline(general_config=config, solver='PSO-RBO', max_time=30).solve(instance_names=['Percentile25'])
-#     Baseline(general_config=config, solver='PSO-RBO').solve(num_replications=5)
+GENERAL_CONFIGS = ['G0', 'G01', 'G1', 'G2', 'G21', 'G3']
 
 for config in GENERAL_CONFIGS:
-    Baseline(general_config=config, solver='PSO-RBO').tune(num_trials=100, num_replications=1)
+    Baseline(general_config=config, solver='PSO-RBO', tuned_hyperparams=True).solve(num_replications=5)
+
+# for config in GENERAL_CONFIGS:
+#     Baseline(general_config=config, solver='PSO', tuned_hyperparams=True).solve(num_replications=5)

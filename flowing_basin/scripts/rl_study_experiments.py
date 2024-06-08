aa21b8de8b15f36e82744dcf946ad67d0aa2ef29
RodrigoCastroF <79080278+RodrigoCastroF@users.noreply.github.com>
2024-06-08 22:08:18 +0200

    Studied Experiment 12

--- flowing_basin/scripts/rl_study_experiments.py
+++ flowing_basin/scripts/rl_study_experiments.py
@@ -28,17 +28,42 @@ def get_experiment11_agents(generals: list[str] = None, rewards: list[str] = Non
     return agents
 
 
+def get_experiment12_agents(general_configs: list[str]) -> list[str]:
+    experiment_agents = [
+        "rl-A31G2O231R1T1402",
+        "rl-A31G3O231R1T1402",
+        "rl-A1G2O231R22T14",
+        "rl-A1G3O231R22T14",
+        "rl-A1G2O2R1T14",
+        "rl-A1G3O2R1T14",
+        "rl-A23G2O3R1T74",
+        "rl-A23G2O3R1T748",
+        "rl-A23G3O3R1T74",
+        "rl-A23G3O3R1T748",
+        "rl-A113G3O2R22T342",
+        "rl-A113G3O2R22T749",
+        "rl-A113G3O232R22T74",
+        "rl-A113G3O232R22T748"
+    ]
+    experiment_agents = [
+        agent for agent in experiment_agents if any(agent.find(gen_config) != -1 for gen_config in general_configs)
+    ]
+    return experiment_agents
+
+
 if __name__ == "__main__":
 
-    general = 'G1'
-    regex = f"rl-A3.{general}"
+    general = 'G3'
+    # regex = f"rl-A3.{general}"
     # agents = ReinforcementLearning.get_all_agents(regex)
-    # print(f"Found {len(agents)} agents in folder:", agents)
-    # ReinforcementLearning.barchart_training_times(regex, hours=False)
+    agents = get_experiment12_agents([general])
+    print(f"Found {len(agents)} agents in folder:", agents)
+    regex = agents
+    # ReinforcementLearning.barchart_training_times(regex, hours=True)
     # ReinforcementLearning.print_training_times(regex, hours=True, csv_filepath="reports/training_times_experiments1-9.csv")
     # ReinforcementLearning.print_max_avg_incomes(
     #     regex, permutation='GOART', baselines=["MILP", "rl-greedy"],
-    #     csv_filepath=f"reports/results_{general}_experiment10.csv"
+    #     csv_filepath=f"reports/results_{general}_experiment12.csv"
     # )
     ReinforcementLearning.plot_all_training_curves(regex, baselines=["MILP", "rl-greedy", "rl-random"])
 

21e8ad5c5ff963bfca3569e1dca9bf0204facc93
RodrigoCastroF <79080278+RodrigoCastroF@users.noreply.github.com>
2024-06-17 10:07:43 +0200

    RL baseline in G2 and G3

--- flowing_basin/scripts/baseline.py
+++ flowing_basin/scripts/baseline.py
@@ -7,8 +7,8 @@ from flowing_basin.solvers import Baseline
 
 GENERAL_CONFIGS = ['G0', 'G01', 'G1', 'G2', 'G21', 'G3']
 
-# (general_config='G0', solver='rl-A31G0O231R1T1002').solve(num_replications=1)
-Baseline(general_config='G1', solver='rl-A113G1O2R22T302').solve(num_replications=1)
+Baseline(general_config='G2', solver='rl-A21G2O3R1T748').solve(num_replications=1)
+Baseline(general_config='G3', solver='rl-A21G3O3R1T74').solve(num_replications=1)
 
 # for config in GENERAL_CONFIGS:
 #     Baseline(general_config=config, solver='PSO-RBO', tuned_hyperparams=True).solve(num_replications=5)
